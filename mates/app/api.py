16adf46edaadc08b789e3868d2f32111fe02a829
JP <jpniznhy@hotmail.com>
2024-10-13 20:16:19 +0200

    Pull into main feature/api (#18)
    * First api template
    
    * Api script
    
    * Add api prediction
    
    * Prediction file creation
    
    * Format for pylint
    
    * Add documentation
    
    ---------
    
    Co-authored-by: Juan Pablo Zaldivar <juan.pablo.zaldivar@estudiantat.com>

--- mates/app/api.py
+++ mates/app/api.py
@@ -1,24 +1,105 @@
-from loguru import logger
+"""
+API module for the Mates application.
+
+This module provides a REST API for dog breed classification using pre-trained models.
+Users can upload images to the API and receive predictions for the dog's breed. The 
+API is built with FastAPI and leverages TensorFlow/Keras models for predictions.
+
+Key Features:
+1. FastAPI Integration: 
+   - Provides an efficient and scalable REST API for real-time predictions and batch 
+     processing of images.
+   
+2. Model Loading and Management: 
+   - Automatically loads all pre-trained models from a specified directory (MODELS_DIR)
+     during the API startup, and stores them in a global dictionary for quick access.
+   
+3. Image Upload and Processing: 
+   - Users can upload dog images in common formats (JPEG, PNG), and the API processes 
+     the image (resizing and scaling) before feeding it into the model for prediction.
+   
+4. Prediction with Pre-trained Models: 
+   - Leverages a fine-tuned MobileNetV3 model for dog breed classification across 
+     over 120 breeds. The predicted breed is returned to the user.
+
+5. Command-line Interface (CLI): 
+   - Additionally, it includes a CLI to batch process test datasets for model 
+     predictions, saving the results as a CSV file for further evaluation.
+
+Dependencies:
+- FastAPI: For building the API and managing requests.
+- Pandas: For handling data outputs and saving results.
+- PIL (Pillow): For image loading and processing.
+- TensorFlow/Keras: For loading pre-trained models and making predictions.
+- Loguru: For structured logging and tracking.
+- Typer: For creating the CLI.
+
+API Workflow:
+1. Model Initialization:
+   - At startup, all `.h5` model files in the MODELS_DIR are loaded into memory and 
+     stored in the `models_dict` dictionary. Labels (dog breeds) associated with 
+     each model are also loaded and stored globally for prediction use.
+   
+2. Image Prediction:
+   - Upon receiving an image via the `/predict` endpoint, the image is preprocessed 
+     (resized to IMG_SIZE, scaled) and passed through the selected pre-trained model 
+     for classification.
+   - The predicted breed label is returned to the client as a JSON response.
+
+3. Error Handling:
+   - If an invalid model name is provided or the uploaded image is corrupt/unsupported, 
+     appropriate error messages are returned using HTTP status codes.
+
+4. Model Listing:
+   - The `/models` endpoint provides a list of all available pre-trained models 
+     for users to select from.
+
+CLI Features:
+- The command-line interface (CLI) allows users to generate predictions on a batch 
+  of test images by specifying a pre-trained model, processing test data, and saving 
+  the results as a CSV file for further analysis.
+
+Key Functions:
+- `predict_test`: 
+   - Loads a specified pre-trained model, processes the test dataset in batches, 
+     generates predictions, and saves the predictions as a CSV file in the output 
+     directory.
+
+- `predict_single`: 
+   - Takes a single image as input, processes it, and returns the predicted dog breed.
+
+API Endpoints:
+- `/`: Root endpoint that returns a welcome message and checks API health.
+- `/models`: Returns a list of all available models for classification.
+- `/predict`: Takes an image and the name of the selected model and returns the 
+  predicted dog breed as a JSON response.
+"""
+
 import logging
 from contextlib import asynccontextmanager
 from http import HTTPStatus
 from typing import List
 
-from fastapi import FastAPI, File, UploadFile, HTTPException
+import pandas as pd
+from fastapi import FastAPI, File, HTTPException, UploadFile
 from fastapi.responses import JSONResponse
-from mates.features import load_model
-from mates.config import MODELS_DIR
+from loguru import logger
 from PIL import Image
 
+from mates.config import MODELS_DIR, RAW_DATA_DIR
+from mates.features import load_model, read_labels
+from mates.modeling.predict import predict_single
 
 # Global variable to store models
 models_dict = {}
+encoding_labels = []
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     """Loads all model files from MODELS_DIR and adds them to models_dict."""
     global models_dict
+    global encoding_labels
 
     # Load all models from MODELS_DIR
     model_paths = [
@@ -28,12 +109,15 @@ async def lifespan(app: FastAPI):
     ]
 
     for model_name in model_paths:
-        models_dict[model_name] = load_model(model_name)
+        models_dict[model_name] = {"model": load_model(model_name)}
+
+    # Load labels
+    _, encoding_labels = read_labels(RAW_DATA_DIR)
 
     yield
 
-    # Clean up models
     models_dict.clear()
+    encoding_labels = []
 
 
 logger.info(f"Initalizing FastAPI application...")
@@ -58,6 +142,7 @@ async def _index():
     }
     return response
 
+
 @app.get("/models", response_model=List[str], tags=["Models"])
 async def _get_models():
     """Endpoint to list all available models"""
@@ -74,27 +159,27 @@ async def _predict_dog_breed(model_name: str, file: UploadFile = File(...)):
             detail=f"Model {model_name} not found. Please choose from available models."
         )
 
-#     try:
-#         image = Image.open(file.file)
-#     except Exception as e:
-#         raise HTTPException(
-#             status_code=HTTPStatus.BAD_REQUEST,
-#             detail=f"Invalid image format: {e}"
-#         )
+    try:
+        image = Image.open(file.file)
+    except Exception as e:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail=f"Invalid image format: {e}"
+        )
 
-#     # Predict breed using the selected model
-#     try:
-#         prediction = predict_breed(models_dict[model_name], image)
-#         return JSONResponse(
-#             status_code=HTTPStatus.OK,
-#             content={
-#                 "model": model_name,
-#                 "prediction": prediction
-#             }
-#         )
-#     except Exception as e:
-#         logger.error(f"Error during prediction: {e}")
-#         raise HTTPException(
-#             status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
-#             detail="An error occurred during prediction."
-#         )
+    # Predict breed using the selected model
+    try:
+        prediction = predict_single(models_dict[model_name]["model"], encoding_labels, image)
+        return JSONResponse(
+            status_code=HTTPStatus.OK,
+            content={
+                "model": model_name,
+                "prediction": prediction
+            }
+        )
+    except Exception as e:
+        logger.error(f"Error during prediction: {e}")
+        raise HTTPException(
+            status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+            detail="An error occurred during prediction."
+        )
--- mates/modeling/prepare.py
+++ mates/modeling/prepare.py
@@ -1,4 +1,37 @@
-"""Module providing a function to prepare the data."""
+"""
+Data Preparation Module
+
+This module provides functionality to prepare data for machine learning tasks such as 
+training and validation. It processes raw image data, performs train-validation splits, 
+and saves the processed datasets for further use. 
+
+Key Features:
+- Configurable data preparation: The parameters, such as train/test mode, validation split size, 
+  and random seed, are loaded from a YAML configuration file.
+- Train-validation split: For training datasets, the module splits the data into training and 
+  validation sets, ensuring proper data separation for model training.
+- Data serialization: Processed datasets, including training, validation, and output shape, 
+  are saved as pickle files for easy reuse without having to reprocess raw data each time.
+
+Workflow:
+1. Load preparation parameters from a configuration file.
+2. Read the raw image data and corresponding labels from the dataset.
+3. If preparing training data:
+   - Split the dataset into training and validation sets based on the configuration.
+   - Save the split datasets and encoding labels.
+4. Log the progress and results of the data preparation.
+
+The module uses the following dependencies:
+- `pickle`: For saving and loading processed datasets.
+- `typer`: For providing a command-line interface (CLI) to execute the data preparation process.
+- `loguru`: For logging information, warnings, and errors during data processing.
+- `sklearn.model_selection.train_test_split`: For splitting data into training and validation sets.
+
+External module imports:
+- `PROCESSED_DATA_DIR` from mates.config: The directory where processed data is saved.
+- `load_params`, `read_data` from mates.features: Functions for loading configuration parameters
+    and reading raw data.
+"""
 
 import pickle as pk
 
@@ -12,22 +45,41 @@ from mates.features import load_params, read_data
 app = typer.Typer()
 
 @app.command()
-def process_data(
-):
+def process_data():
     """
-    Function to process data. Performs data processing and saves processed data.
+    Process and prepare data for training or validation. This function reads raw data,
+    performs a train-validation split if required, and saves the processed data.
+
+    The function reads parameters from a configuration file, such as whether the data
+    is for training or testing, the size of the validation split, and whether to save 
+    the processed data.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
     """
+    # Load preparation parameters from the config
     params = load_params("prepare")
 
     logger.info("Processing data...")
+
+    # Read raw data (for training or testing based on params)
     x, y, encoding_labels = read_data(train_data=params["is_train"])
 
+    # If training data is being processed
     if params["is_train"]:
         output_shape = len(encoding_labels)
+
+        # Perform train-validation split
         x_train, x_val, y_train, y_val = train_test_split(
             x, y, test_size=params["split_size"], random_state=params["seed"]
         )
 
+        # Save processed data if specified in the params
         if params["save_processed"]:
             with open(PROCESSED_DATA_DIR / 'output_shape.pkl', 'wb') as f:
                 pk.dump(output_shape, f)
@@ -42,6 +94,7 @@ def process_data(
                 pk.dump(y_val, f)
 
     logger.success("Data processing complete.")
+
 
 if __name__ == "__main__":
     process_data()
