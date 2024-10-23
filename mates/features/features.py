6d1ac55bd7ff4fc1705783d7d47db70993fbb931
AndreaTomas00 <andrea.tomas00@gmail.com>
2024-10-23 19:13:48 +0200

    test fix

--- mates/features/features.py
+++ mates/features/features.py
@@ -202,12 +202,11 @@ def read_data(
     data_type = "train" if train_data else "test"
     imgs = os.listdir(dir_path / f"{data_type}/")
 
-    x = [dir_path / f"{data_type}/" / f for f in imgs]
-
     if train_data:
         labels, encoding_labels = read_labels(dir_path)
         y = pd.get_dummies(labels["breed"]).to_numpy()
-        encoding_labels = labels["breed"].unique()
+        x = [dir_path / f'{data_type}/' / f'{id}.jpg' for id in labels['id']]
+        encoding_labels = labels['breed'].unique()
     else:
         imgs = os.listdir(dir_path / f"{data_type}/")
         x = [dir_path / f"{data_type}/" / f for f in imgs]
